b906de7c4a7f2bf141d3b55c1887055e29ee3502
mkswagger <mathangykrishna16@gmail.com>
2024-03-30 16:40:37 +0530

    launchpad changed , zsh modified

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,64 +2,28 @@ import type { LaunchpadData } from "~/types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
+    id: "raasta",
+    title: "Raasta",
+    img: "img/icons/launchpad/raasta.png",
+    link: "https://github.com/mkswagger/Raasta"
   },
   {
-    id: "oh-my-cv",
-    title: "Oh, My CV!",
-    img: "https://ohmycv.app/pwa-512x512.png",
-    link: "https://ohmycv.app"
+    id: "jeevan",
+    title: "G-one(Jeevan)",
+    img: "img/icons/launchpad/geevan.png",
+    link: "https://github.com/mkswagger/G-ONE_DSHACK"
   },
   {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/oh-vue-icons.png",
-    link: "https://oh-vue-icons.js.org"
+    id: "reviewroom",
+    title: "Review Room",
+    img: "img/icons/launchpad/reviewroom.png",
+    link: "https://github.com/mkswagger/review_room_mozohack4.0"
   },
   {
-    id: "gungnir",
-    title: "Gungnir",
-    img: "img/icons/launchpad/gungnir.png",
-    link: "https://vuepress-theme-gungnir.vercel.app/"
-  },
-  // {
-  //   id: "metallic",
-  //   title: "Metallic",
-  //   img: "img/icons/launchpad/meta.png",
-  //   link: "https://github.com/Renovamen/metallic"
-  // },
-  {
-    id: "what-if",
-    title: "My Notebook",
-    img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
-  },
-  {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
-  },
-  {
-    id: "resume",
-    title: "Résumé",
-    img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
-  },
-  {
-    id: "zelda",
-    title: "Webpage for Zelda",
-    img: "img/icons/launchpad/zelda.png",
-    link: "https://zelda.zxh.io/"
-  },
-  {
-    id: "fishmail",
-    title: "Fishmail",
-    img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
+    id: "portfolio-macos",
+    title: "Portfolio",
+    img: "img/icons/launchpad/portfolio.png",
+    link: ""
   }
 ];
 
